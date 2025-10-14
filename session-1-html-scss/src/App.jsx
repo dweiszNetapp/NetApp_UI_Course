c97808ad3f26dbbb4da75bc6e7197c7d1e546794
dweisz <dweisz@netapp.com>
2025-10-14 11:12:31 +0300

    Memory app

---
+++ session-1-html-scss/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.scss';
+import './App-vp.scss';
+import { ReactComponent as MemoryIcon } from './assets/memoryApp.svg';
+
+function App() {
+  return (
+    <div className='memoryAppContainer'></div>
+  )
+}
+
+export default App
---
+++ session-3-typescript/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import svgr from '@svgr/rollup';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+  svgr({ plugins: ['@svgr/plugin-jsx'] })
+  ],
+})
