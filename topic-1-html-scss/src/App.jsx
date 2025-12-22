c7c3b7dac3110d3fe8f944c805211ccb875ee39c
dweiszNetapp <dweisz@netapp.com>
2025-12-22 12:46:25 +0200

    topic-1-html-scss-session-3

--- topic-1-html-scss/src/App.jsx
+++ topic-1-html-scss/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import './App-vp.scss';
 import { ReactComponent as MemoryIcon } from './assets/memoryApp.svg';
 
@@ -6,20 +6,27 @@ function App() {
   return (
     <div className='memoryAppContainer'>
       <div className='momoryTitle'>
-        <div className='momoryTitle titleLeftSide'>
+        <div className='momoryTitle'>
           <MemoryIcon className='memoryIcon' />
-          <div className='titleBlck'>
+          <div className='titleBlock'>
             <span>Difficulty level</span>
             <input />
           </div>
-          <div className='titleBlck'>
+          <div className='titleBlock'>
             <span>Best score</span>
             <span>Number of mistaks: 0</span>
           </div>
         </div>
         <button>Reset game</button>
       </div>
-      <div>Content</div>
+      <div className='memoryGameContent'>
+        <div className="tile">4</div>
+        <div className="tile">2</div>
+        <div className="tile">12</div>
+        <div className="tile">8</div>
+        <div className="tile">9</div>
+        <div className="tile last">5</div>
+      </div>
     </div>
   )
 }
